bb6bcf83cbd5485ff2ec8e55b493ce71a9a97478
SecretAI <rar3mayhem@gmail.com>
2022-06-12 15:04:39 +0700

    Update auth tests

--- api/src/test/unit/auth/_login.test.ts
+++ api/src/test/unit/auth/_login.test.ts
@@ -2,17 +2,34 @@ import { testAcc } from "..";
 import pg from "../../../database/index";
 import { TokenInstance } from "../../../database/models/token/instance";
 import { RefreshToken } from "../../../database/models/token/interfaces";
+import { UserInstance } from "../../../database/models/users/instance";
 import { AuthService } from "../../../service/auth";
 import { JwtTokenPair } from "../../../service/auth/interfaces";
 
 
 describe("Login Demo", () => {  
-
-  afterAll(async () => {
+  async function removeTestData() {
     await pg.createQuery(`
+      BEGIN;
+
       DELETE FROM users
       WHERE email = '${testAcc.email}';
+
+      DELETE FROM tokens
+      WHERE user_email = '${testAcc.email}';
+
+      END;
     `);
+  }
+
+  beforeAll(async () => {
+    removeTestData();
+    await AuthService.createNewAccount(testAcc);
+    console.info("[Test:Login] BeforeAll hook - success");
+  });
+
+  afterAll(async () => {
+    removeTestData();    
     console.info("[Test:Login] AfterAll hook - success");
   });
 
--- api/src/test/unit/auth/_signup.test.ts
+++ api/src/test/unit/auth/_signup.test.ts
@@ -4,7 +4,6 @@ import { User } from "../../../database/models/users/interfaces";
 import { AuthService } from "../../../service/auth";
 
 describe("SignUp Demo", () => {
-
   beforeAll(async () => {
     await pg.createQuery(`
       DELETE FROM users
@@ -13,10 +12,18 @@ describe("SignUp Demo", () => {
     console.info("[Test:Signup] BeforeAll hook - success");
   });
 
+  afterAll(async () => {
+    await pg.createQuery(`
+      DELETE FROM users
+      WHERE email = '${testAcc.email}';
+    `);
+    console.info("[Test:Signup] AfterAll hook - success");
+  });
+
   it("Should create a new User", async () => {
     const user: User = await AuthService.createNewAccount(testAcc);
   
     expect(user).toBeTruthy();
     expect(user.password).not.toBe(testAcc.password); /* Password should now be encrypted */
-  })
+  });
 });
